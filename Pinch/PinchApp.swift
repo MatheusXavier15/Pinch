8e875f6fe2d1f4f17d665c022083128727012011
Matheus Xavier <matheus.oliveira@rhitmo.com.br>
2023-07-29 15:56:59 -0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Matheus Xavier on 29/07/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
